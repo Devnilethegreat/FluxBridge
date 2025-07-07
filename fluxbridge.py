72b3fb3e5c0049e7d913132ac6ee12c6e43d40ec
Devnilethegreat <212276580+Devnilethegreat@users.noreply.github.com>
2025-07-07 17:15:18 +0000

    Scaffold core module structure

---
+++ fluxbridge.py
@@ -0,0 +1,14 @@
+# fluxbridge.py
+"""
+Main module for FluxBridge application.
+"""
+
+import argparse
+import logging
+import os
+import sys
+from typing import Optional
+from dotenv import load_dotenv
+
+load_dotenv()
+
